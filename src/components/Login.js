3bf9d3e0b0225bb0d17e23bc27d67cf72a46b831
Harman Tiwana <harmant901@gmail.com>
2020-12-04 12:03:00 -0800

    testing

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ class Login extends React.Component {
             <div>
             
                 <h2>Login</h2>
-                <form method="POST" action="/user/login">
+                <form method="POST" action="/ks">
                     <input type="text" name="username" placeholder="username"></input><br/>
                     <input type="text" name="password" placeholder="password"></input><br/>
                     <input value="Login" type="submit"></input>
