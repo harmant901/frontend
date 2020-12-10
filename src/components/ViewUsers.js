e914b33f52ad2ddf7571670fe542149bff60710a
Harman Tiwana <harmant901@gmail.com>
2020-12-09 21:49:20 -0800

    Added a view user component

---
+++ src/components/ViewUsers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ViewUsers extends React.Component {
+    render() {
+        return (
+            <h1>List of users  </h1>
+        );
+    }
+}
+
+export default ViewUsers;
