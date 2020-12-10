a20f22923796af1560af25fd334538093455fdda
Harman Tiwana <harmant901@gmail.com>
2020-12-10 13:20:12 -0800

    Added list users function at home screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import Login from './components/Login';
 import Register from './components/Register';
+import ViewUsers from './components/ViewUsers';
 import {
   BrowserRouter as Router,
   Link,
@@ -14,13 +15,14 @@ function App() {
       <h1>Welcome to Harmans List</h1>
       <Router>
       <Switch>
-        
+        <Route path='/viewusers' component={ViewUsers} exact />
         <Route path="/register" component={Register} exact />
         <Route path="/login" component={Login} exact />
       </Switch>
       
       <Link className="link" to="/register">Click me to Register</Link><br></br>
-      <Link className="link" to="/login">Click me to Login</Link>
+      <Link className="link" to="/login">Click me to Login</Link><br></br>
+      <Link className="link" to="/viewusers">Click me to view all registered users</Link>
       
     
       </Router>
--- src/components/ViewUsers.js
+++ src/components/ViewUsers.js
@@ -1,9 +1,57 @@
 import React from 'react';
 
 class ViewUsers extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userData: [
+                {
+                    
+                }
+            ]
+            
+        }
+
+        this.fetchData = this.fetchData.bind(this);
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+    
+    fetchData() {
+        fetch('http://localhost:8000/users',
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }
+        )
+        .then(response => response.json())
+        .then(json => {
+            this.setState({userData: json})
+            console.log(this.state.userData);
+            
+        });
+
+      //  this.setState({userData: "Harmssa"})
+    }
+
+
+
     render() {
+        
         return (
-            <h1>List of users  </h1>
+            
+            <div>
+                <h2>List of users</h2>
+                <ul>
+                    {this.state.userData.map(d => (<li key={d.username}>{d.username}</li>))}
+                </ul>
+               
+            </div>
         );
     }
 }
