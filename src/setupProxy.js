b41c47bf7eef247a8eb581ad7949811180d57b88
Harman Tiwana <harmant901@gmail.com>
2020-12-10 19:31:27 -0800

    New

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,3 +1,4 @@
+/*
 const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
@@ -5,3 +6,5 @@ module.exports = function(app) {
     proxy(["/"], { target: "http://localhost:8000/" })
   );
 };
+
+*/
