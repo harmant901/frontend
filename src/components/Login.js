b29264c45a96e4827c544115e55078ada2d2d18e
Harman Tiwana <harmant901@gmail.com>
2020-12-10 23:00:28 -0800

    errors fixed

--- src/components/Login.js
+++ src/components/Login.js
@@ -24,7 +24,7 @@ class Login extends React.Component {
         Axios.post('http://54.226.49.231:8000/user/login', this.state)
         .then(response => {
             console.log(response)
-            location.href = 'http://54.226.49.231:8000/setcookies';
+            window.href = 'http://54.226.49.231:8000/setcookies';
         })
         .catch(error => {
             console.log(error);
