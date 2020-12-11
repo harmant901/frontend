95f449de66c2364b29bfaac5c77ecae8df67897c
Harman Tiwana <harmant901@gmail.com>
2020-12-10 21:22:19 -0800

    Some testing

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,50 @@
 import React from 'react';
+import Axios from 'axios';
 import './LoginRegister.css';
 
 class Login extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            username: "",
+            password: ""
+        }
     }
+
+    handleChangeUsername = (e) => {
+        this.setState({username: e.target.value})
+    }
+
+    handleChangePassword = (e) => {
+        this.setState({password: e.target.value})
+    }
+
+    handleClick = () => {
+        console.log(this.state);
+        Axios.post('http://localhost:8000/user/login', this.state)
+        .then(response => {
+            console.log(response)
+            window.location = 'http://localhost:8000/setcookies';
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+
+    sendPostData = () => {
+
+    }
+
     render() {
         return (
             <div>
                 <center>
                 <h2>Login</h2>
                 <form method="POST" action="/user/login">
-                    <input type="text" name="username" placeholder="username"></input><br/>
-                    <input type="text" name="password" placeholder="password"></input><br/>
-                    <input value="Login" type="submit"></input>
+                    <input type="text" onChange={this.handleChangeUsername} name="username" placeholder="username"></input><br/>
+                    <input type="text" onChange={this.handleChangePassword} name="password" placeholder="password"></input><br/>
+                    <button value="Login" type="button" onClick={this.handleClick}>Login</button>
                 </form>
                 </center>
             </div>
